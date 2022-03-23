dacefbe4b4c8251a31c7fbd56453e2c2abb9e1ff
williamkhoo <william_khoo@outlook.com>
2022-03-24 00:10:42 +0800

    update

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,9 @@
 import { JwtPayload } from "./jwt-payload";
 import { AuthService } from "./auth.service";
 import { AuthModule } from "./auth.module";
+import JwtConfig from "./config/jwt.config";
 
-export { JwtPayload, AuthService, AuthModule };
+export { JwtPayload, AuthService, AuthModule, JwtConfig };
 export * from "./decorator";
 export * from "./guard";
 export * from "./strategy";
